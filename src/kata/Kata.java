34c8807a4099bbf05fd88315799b38490528d889
samuelgbenga <samuelgbenga972@gmail.com>
2026-03-20 16:45:49 +0100

    done with the issquare

--- src/kata/Kata.java
+++ src/kata/Kata.java
@@ -5,8 +5,8 @@ public class Kata {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        System.out.println(isPrimeNumber(-2));
-        //isPrimeNumber(2);
+        System.out.println(isSquare(400));
+
     }
 
     public static boolean isEven(int number) {
@@ -20,5 +20,38 @@ public class Kata {
             if(number % i == 0) return false;
         }
         return true;
+    }
+
+    public static int subtract(int num1, int num2){
+        int result = num1 - num2;
+        return Math.abs(result);
+    }
+
+    public static float divide(int a, int b) {
+        return b == 0 ? 0 : (float) a / b;
+    }
+
+    public static int factorOf(int number) {
+        if (number <= 0) return 0;
+
+        int count = 0;
+
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static boolean isSquare(int number) {
+        if (number < 0) return false;
+
+        for (int i = 0; i <= Math.sqrt(number); i++) {
+            if (i * i == number) return true;
+        }
+
+        return false;
     }
 }
