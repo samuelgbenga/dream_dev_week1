7db724a8581c1f8a7b6dbaf8a7020d3d4bc3889f
samuelgbenga <samuelgbenga972@gmail.com>
2026-03-21 17:36:17 +0100

    validity check feature

--- src/validatecreditcard/ValidateCreditCard.java
+++ src/validatecreditcard/ValidateCreditCard.java
@@ -1,9 +1,111 @@
 package validatecreditcard;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class ValidateCreditCard {
 
+    static final int VISA_CARD = 4;
+    static final int MASTER_CARD = 5;
+    static final int AMERICAN_EXPRESS_CARDS = 37;
+    static final int DISCOVER_CARDS = 6;
+    static String cardType;
+    static String cardNumber ="4388576018402626";
+
+
 
     public static void main(String[] args) {
+        //String input1 = "12345254467864";
+         Scanner input = new Scanner(System.in);
 
+
+
+
+        System.out.println(Arrays.toString(stringToIntArray()));
+        System.out.println(sumDoubleEveryOtherFromRight(stringToIntArray()));
+        System.out.println(sumOddIndexedDigitsFromRightToLeft(stringToIntArray()));
+
+
+//        int cardPrefix = 37; // example
+//        setCardType(cardPrefix);
+//        System.out.println("Card Type: " + CARD_TYPE);
+
+//        int result = addUntilSingleDigit(7, 8); // 7 + 8 = 15 -> 1 + 5 = 6
+//        System.out.println(result); // Output: 6
+        //System.out.println(cardReportTemplate("Valid")); // Output: 6
+    }
+
+
+    private static int[] stringToIntArray() {
+
+        int[] result = new int[cardNumber.length()];
+
+        for (int i = 0; i < cardNumber.length(); i++) {
+
+            result[i] = Character.getNumericValue(cardNumber.charAt(i));
+        }
+
+        return result;
+    }
+
+    public static void setCardType(int cardPrefix) {
+        cardType = switch (cardPrefix) {
+            case VISA_CARD -> "Visa";
+            case MASTER_CARD -> "MasterCard";
+            case DISCOVER_CARDS -> "Discover";
+            case AMERICAN_EXPRESS_CARDS -> "American Express";
+            default -> "Invalid Card";
+        };
+    }
+
+    private static int addUntilSingleDigit(int a, int b) {
+        int sum = a + b;
+        if (sum > 9) return addUntilSingleDigit(sum / 10, sum % 10);
+        return sum;
+    }
+
+    private static String cardReportTemplate(String isValid){
+        return String.format("""
+        ************************************************
+        ** Credit Card Type : %s
+        ** Credit Card Number : %s
+        ** Credit Card Digit length : %d
+        ** Credit Card Validity Status : %s
+        ************************************************
+        """,
+        cardType,
+        cardNumber,
+        cardNumber.length(),
+        isValid
+        );
+    }
+
+
+    private static int sumDoubleEveryOtherFromRight(int[] digits) {
+        int sum = 0;
+        int n = digits.length;
+
+
+        for (int i = n - 2; i >= 0; i -= 2) {
+            int a = digits[i] * 2;
+
+            if(a > 9){
+                a = addUntilSingleDigit(digits[i], digits[i]);
+            }
+
+            sum += a;
+        }
+
+        return sum;
+    }
+
+    private static int sumOddIndexedDigitsFromRightToLeft(int[] digits) {
+        int sum = 0;
+        int n = digits.length;
+
+
+        for (int i = n - 1; i >= 0; i -= 2) {
+            sum += digits[i];
+        }
+        return sum;
     }
 }
